9d91c46273f9de96b3f516c80a6da992eb0ed615
Christopher Byrd <christopher.byrd2013@gmail.com>
2018-03-08 16:04:49 -0600

    Prepped 'save-to-database' portion to pick up where 'save-to-file' will leave off.

--- save-to-database/create_table.sql
+++ save-to-database/create_table.sql
@@ -1,9 +1,7 @@
--- 
 -- DDL statement for creating a simple SQLite table
--- 
 CREATE TABLE IF NOT EXISTS books (
   id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL DEFAULT '',
+  name TEXT NOT NULL,
   url TEXT,
   upc TEXT,
   price REAL NOT NULL DEFAULT 0.00,
--- save-to-database/main.py
+++ save-to-database/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Simple program to illustrate scraping web data and saving it to a databse
+"""Simple program to illustrate scraping web data and saving it to a database
 """
 
 import requests
 import sys
+from book import Book
 from bs4 import BeautifulSoup
 
 
@@ -12,8 +13,10 @@ URL = 'http://books.toscrape.com'
 page = requests.get(URL)
 
 if page.status_code != 200:
-    print("{} status code received.\nExiting.".format(page.status_code))
+    print('{} status code received.\nExiting.'.format(page.status_code))
     sys.exit(1)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+
+product_elements = soup.find_all('article', {'class': 'product_pod'})
 
